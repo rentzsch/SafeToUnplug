ef3002b652258697f7bb8a403df7a1fc09840333
rentzsch <jwr.git@redshed.net>
2012-10-30 17:55:50 -0500

    Initial NSWorkspace implementation working.

--- SafeToUnplug/AppDelegate.m
+++ SafeToUnplug/AppDelegate.m
@@ -10,9 +10,25 @@
 
 @implementation AppDelegate
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
-{
-    // Insert code here to initialize your application
+- (void)applicationDidFinishLaunching:(NSNotification*)notification {
+    // ref: http://stackoverflow.com/questions/1408216/detect-when-removable-storage-is-unmounted
+    
+    NSNotificationCenter *workpaceNotificationCenter = [[NSWorkspace sharedWorkspace] notificationCenter];
+    
+    [workpaceNotificationCenter addObserver:self
+                                   selector:@selector(didUnmountNotification:)
+                                       name:NSWorkspaceDidUnmountNotification
+                                     object:nil];
+}
+
+- (void)didUnmountNotification:(NSNotification*)notification {
+    NSLog(@"%@ %@", notification, [notification userInfo]);
+    
+    NSUserNotification *userNote = [NSUserNotification new];
+    userNote.title = @"Safe to Unplug";
+    userNote.informativeText = [[notification userInfo] objectForKey:NSWorkspaceVolumeLocalizedNameKey];
+    userNote.hasActionButton = NO;
+    [[NSUserNotificationCenter defaultUserNotificationCenter] deliverNotification:userNote];
 }
 
 @end
